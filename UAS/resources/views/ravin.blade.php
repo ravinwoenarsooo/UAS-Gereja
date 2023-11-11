7cb0ee21b0b6766d018483637b16e34e7f6417e2
ravinwoenarsooo <robertus.ravin@student.umn.ac.id>
2023-11-11 14:57:15 +0700

    Update UAS!
    masih basic

--- UAS/resources/views/ravin.blade.php
+++ UAS/resources/views/ravin.blade.php
@@ -1,13 +1,70 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-</head>
-<body>
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-    </h1>
-</body>
-</html>
+@extends('layouts.home')
+
+@section('content')
+<section class="text-gray-400 bg-gray-900 body-font">
+    <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center" bis_skin_checked="1">
+        <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0 mb-10" bis_skin_checked="1">
+        <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600">
+        </div>
+        <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center" bis_skin_checked="1">
+        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Before they sold out
+            <br class="hidden lg:inline-block">readymade gluten
+        </h1>
+        <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+        <div class="flex justify-center" bis_skin_checked="1">
+            <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+            <button class="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">Button</button>
+        </div>
+        </div>
+    </div>
+    <div class="container px-5 py-24 mx-auto" bis_skin_checked="1">
+        <div class="flex flex-col" bis_skin_checked="1">
+        <div class="h-1 bg-gray-800 rounded overflow-hidden" bis_skin_checked="1">
+            <div class="w-24 h-full bg-indigo-500" bis_skin_checked="1"></div>
+        </div>
+        <div class="flex flex-wrap sm:flex-row flex-col py-6 mb-12" bis_skin_checked="1">
+            <h1 class="sm:w-2/5 text-white font-medium title-font text-2xl mb-2 sm:mb-0">Space The Final Frontier</h1>
+            <p class="sm:w-3/5 leading-relaxed text-base sm:pl-10 pl-0">Street art subway tile salvia four dollar toast bitters selfies quinoa yuccie synth meditation iPhone intelligentsia prism tofu. Viral gochujang bitters dreamcatcher.</p>
+        </div>
+        </div>
+        <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4" bis_skin_checked="1">
+        <div class="p-4 md:w-1/3 sm:mb-0 mb-6" bis_skin_checked="1">
+            <div class="rounded-lg h-64 overflow-hidden" bis_skin_checked="1">
+            <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1203x503">
+            </div>
+            <h2 class="text-xl font-medium title-font text-white mt-5">Shooting Stars</h2>
+            <p class="text-base leading-relaxed mt-2">Swag shoindxgoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+            <a class="text-indigo-400 inline-flex items-center mt-3">Learn More
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+            </a>
+        </div>
+        <div class="p-4 md:w-1/3 sm:mb-0 mb-6" bis_skin_checked="1">
+            <div class="rounded-lg h-64 overflow-hidden" bis_skin_checked="1">
+            <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1204x504">
+            </div>
+            <h2 class="text-xl font-medium title-font text-white mt-5">The Catalyzer</h2>
+            <p class="text-base leading-relaxed mt-2">Swag shoindxigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+            <a class="text-indigo-400 inline-flex items-center mt-3">Learn More
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+            </a>
+        </div>
+        <div class="p-4 md:w-1/3 sm:mb-0 mb-6" bis_skin_checked="1">
+            <div class="rounded-lg h-64 overflow-hidden" bis_skin_checked="1">
+            <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1205x505">
+            </div>
+            <h2 class="text-xl font-medium title-font text-white mt-5">The 400 Blows</h2>
+            <p class="text-base leading-relaxed mt-2">Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+            <a class="text-indigo-400 inline-flex items-center mt-3">Learn More
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+            </a>
+        </div>
+        </div>
+    </div>
+</section>
+@endsection
