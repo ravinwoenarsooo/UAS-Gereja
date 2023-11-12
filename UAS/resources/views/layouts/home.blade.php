e1fd388e01fd5d3aaa5c8e8ede19a0917d72cffd
ravinwoenarsooo <robertus.ravin@student.umn.ac.id>
2023-11-12 21:29:39 +0700

    Update
    Tailwind

--- UAS/resources/views/layouts/home.blade.php
+++ UAS/resources/views/layouts/home.blade.php
@@ -18,7 +18,7 @@
                 <a class="mr-5 hover:text-white">Third Link</a>
                 <a class="mr-5 hover:text-white">Fourth Link</a>
             </nav>
-            <button class="inline-flex items-center bg-orange-00 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Button
+            <button class="inline-flex items-center bg-orange-00 border-0 py-1 px-3 focus:outline-none hover:bg-orange-200 hover:text-black rounded text-base mt-4 md:mt-0">Home
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
@@ -30,7 +30,7 @@
         @yield('content')
     </main>
 
-    <footer class="text-gray-400 bg-gray-900 body-font">
+    <footer class="text-gray-400 bg-orange-300 body-font">
         <div class="container px-5 py-24 mx-auto" bis_skin_checked="1">
             <div class="flex flex-wrap md:text-left text-center order-first" bis_skin_checked="1">
             <h1 class="sm:text-3xl text-2xl font-medium title-font text-left text-white mb-20">Paroki Pamulang
@@ -52,32 +52,32 @@
                 </div> -->
             </div>
         </div>
-        <div class="bg-gray-800 bg-opacity-75" bis_skin_checked="1">
+        <div class="bg-orange-100 bg-opacity-75" bis_skin_checked="1">
             <div class="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col" bis_skin_checked="1">
                 <a class="flex title-font font-medium items-center md:justify-start justify-center text-white">
                     <img src="{{ asset('images/paroki.png') }}" alt="Paroki Logo" class="w-10 h-10 rounded-full">
                 </a>
-                <p class="text-sm text-gray-400 sm:ml-6 sm:mt-0 mt-4">Copyright © 2023</p>
-                <p class="text-sm text-gray-400 font-bold sm:ml-6 sm:mt-0 mt-4">Komsos St.Barnabas Pamulang</p>
-                <p class="text-sm text-gray-400 sm:ml-6 sm:mt-0 mt-4">All Right Reserved.</p>
+                <p class="text-sm text-black sm:ml-6 sm:mt-0 mt-4">Copyright © 2023</p>
+                <p class="text-sm text-black font-bold sm:ml-6 sm:mt-0 mt-4">Komsos St.Barnabas Pamulang</p>
+                <p class="text-sm text-black sm:ml-6 sm:mt-0 mt-4">All Right Reserved.</p>
                 <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                    <a class="text-gray-400">
+                    <a class="text-black">
                         <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                             <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                         </svg>
                     </a>
-                    <a class="ml-3 text-gray-400">
+                    <a class="ml-3 text-black">
                         <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                             <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                         </svg>
                     </a>
-                    <a class="ml-3 text-gray-400">
+                    <a class="ml-3 text-black">
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                             <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                             <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                         </svg>
                     </a>
-                    <a class="ml-3 text-gray-400">
+                    <a class="ml-3 text-black">
                         <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
                             <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
                             <circle cx="4" cy="4" r="2" stroke="none"></circle>
--- UAS/resources/views/ravin.blade.php
+++ UAS/resources/views/ravin.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.home')
 
 @section('content')
-<section class="text-gray-400 bg-gray-900 body-font">
+<section class="text-black bg-orange-100 body-font">
     <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center" bis_skin_checked="1">
         <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0 mb-10" bis_skin_checked="1">
         <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600">
         </div>
         <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center" bis_skin_checked="1">
-        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Before they sold out
+        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">Before they sold out
             <br class="hidden lg:inline-block">readymade gluten
         </h1>
         <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
@@ -18,17 +18,17 @@
         </div>
     </div>
     <div class="container px-5 py-24 mx-auto">
-        <h1 class="sm:text-3xl text-2xl font-medium title-font text-center text-white mb-20">Jadwal Misa
+        <h1 class="sm:text-3xl text-2xl font-medium title-font text-center text-black mb-20">Jadwal Misa
             <br class="hidden sm:block">Gereja Santo Barnabas
         </h1>
         <div class="-my-8 divide-y-2 divide-gray-800">
         <div class="py-8 flex flex-wrap md:flex-nowrap">
             <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-            <span class="font-semibold title-font text-white">Misa Harian</span>
-            <span class="mt-1 text-gray-500 text-sm">Gereja Katolik Santo Barnabas</span>
+            <span class="font-semibold title-font text-black">Misa Harian</span>
+            <span class="mt-1 text-black text-sm">Gereja Katolik Santo Barnabas</span>
             </div>
             <div class="md:flex-grow">
-            <h2 class="text-2xl font-medium text-white title-font mb-2">Jam 06.00 WIB</h2>
+            <h2 class="text-2xl font-medium text-black title-font mb-2">Jam 06.00 WIB</h2>
             <p class="leading-relaxed">Perayaan Ekaristi yang dilakukan setiap hari Senin - Jumat di Gereja Katolik Santo Barnabas.</p>
             <!-- <a class="text-indigo-400 inline-flex items-center mt-4">Learn More
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -40,11 +40,11 @@
         </div>
         <div class="py-8 flex border-t-2 border-gray-800 flex-wrap md:flex-nowrap">
             <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-            <span class="font-semibold title-font text-white">Misa Jumat Pertama</span>
-            <span class="mt-1 text-gray-500 text-sm">Gereja Katolik Santo Barnabas</span>
+            <span class="font-semibold title-font text-black">Misa Jumat Pertama</span>
+            <span class="mt-1 text-black text-sm">Gereja Katolik Santo Barnabas</span>
             </div>
             <div class="md:flex-grow">
-            <h2 class="text-2xl font-medium text-white title-font mb-2">Jam 18.00 WIB</h2>
+            <h2 class="text-2xl font-medium text-black title-font mb-2">Jam 18.00 WIB</h2>
             <p class="leading-relaxed">Perayaan Ekaristi yang dilakukan pada setiap awal bulan di Gereja Katolik Santo Barnabas.</p>
             <!-- <a class="text-indigo-400 inline-flex items-center mt-4">Learn More
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -56,11 +56,11 @@
         </div>
         <div class="py-8 flex border-t-2 border-gray-800 flex-wrap md:flex-nowrap">
             <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-            <span class="font-semibold title-font text-white">Misa Sabtu</span>
-            <span class="mt-1 text-gray-500 text-sm">Gereja Katolik Santo Barnabas</span>
+            <span class="font-semibold title-font text-black">Misa Sabtu</span>
+            <span class="mt-1 text-black text-sm">Gereja Katolik Santo Barnabas</span>
             </div>
             <div class="md:flex-grow">
-            <h2 class="text-2xl font-medium text-white title-font mb-2">Jam 17.00 WIB</h2>
+            <h2 class="text-2xl font-medium text-black title-font mb-2">Jam 17.00 WIB</h2>
             <p class="leading-relaxed">Perayaan Ekaristi yang dilakukan setiap Sabtu di Gereja Katolik Santo Barnabas.</p>
             <!-- <a class="text-indigo-400 inline-flex items-center mt-4">Learn More
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -72,14 +72,14 @@
         </div>
         <div class="py-8 flex border-t-2 border-gray-800 flex-wrap md:flex-nowrap">
             <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-            <span class="font-semibold title-font text-white">Misa Minggu</span>
-            <span class="mt-1 text-gray-500 text-sm">Gereja Katolik Santo Barnabas</span>
+            <span class="font-semibold title-font text-black">Misa Minggu</span>
+            <span class="mt-1 text-black text-sm">Gereja Katolik Santo Barnabas</span>
             </div>
             <div class="md:flex-grow">
-            <h2 class="text-2xl font-medium text-white title-font mb-2">Jam 05.30 WIB</h2>
-            <h2 class="text-2xl font-medium text-white title-font mb-2">Jam 07.30 WIB</h2>
-            <h2 class="text-2xl font-medium text-white title-font mb-2">Jam 10.00 WIB</h2>
-            <h2 class="text-2xl font-medium text-white title-font mb-2">Jam 17.00 WIB</h2>
+            <h2 class="text-2xl font-medium text-black title-font mb-2">Jam 05.30 WIB</h2>
+            <h2 class="text-2xl font-medium text-black title-font mb-2">Jam 07.30 WIB</h2>
+            <h2 class="text-2xl font-medium text-black title-font mb-2">Jam 10.00 WIB</h2>
+            <h2 class="text-2xl font-medium text-black title-font mb-2">Jam 17.00 WIB</h2>
             <p class="leading-relaxed">Perayaan Ekaristi yang dilakukan setiap Minggu di Gereja Katolik Santo Barnabas.</p>
             <!-- <a class="text-indigo-400 inline-flex items-center mt-4">Learn More
                 <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -93,10 +93,10 @@
     <div class="container px-5 py-24 mx-auto">
         <div class="flex flex-wrap w-full mb-20">
         <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
-            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">Warta Paroki</h1>
-            <div class="h-1 w-20 bg-indigo-500 rounded"></div>
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-black">Warta Paroki</h1>
+            <div class="h-1 w-20 bg-yellow-950 rounded"></div>
         </div>
-        <p class="lg:w-1/2 w-full leading-relaxed text-gray-400 text-opacity-90">Selamat datang di edisi terbaru Warta Paroki Santo Barnabas! Dalam bulan yang penuh berkat ini, kami ingin berbagi berbagai kegiatan dan peristiwa yang mengisi hidup berjemaat. Gereja Santo Barnabas terus menjadi tempat yang penuh kasih dan rohaniah, menginspirasi umat untuk hidup dalam iman dan pelayanan.</p>
+        <p class="lg:w-1/2 w-full leading-relaxed text-black text-opacity-90">Selamat datang di edisi terbaru Warta Paroki Santo Barnabas! Dalam bulan yang penuh berkat ini, kami ingin berbagi berbagai kegiatan dan peristiwa yang mengisi hidup berjemaat. Gereja Santo Barnabas terus menjadi tempat yang penuh kasih dan rohaniah, menginspirasi umat untuk hidup dalam iman dan pelayanan.</p>
         </div>
         <div class="flex flex-wrap -m-4">
         <div class="xl:w-1/4 md:w-1/2 p-4">
