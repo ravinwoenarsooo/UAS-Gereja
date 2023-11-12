8cca3870dea280089107b7b2e3bdb0d70dc9a53c
ravinwoenarsooo <robertus.ravin@student.umn.ac.id>
2023-11-12 20:44:11 +0700

    Update HOME!
    Ubah isi konten home,
    Nambah foto logo di direktori Public.

--- UAS/resources/views/ravin.blade.php
+++ UAS/resources/views/ravin.blade.php
@@ -17,52 +17,119 @@
         </div>
         </div>
     </div>
-    <div class="container px-5 py-24 mx-auto" bis_skin_checked="1">
-        <div class="flex flex-col" bis_skin_checked="1">
-        <div class="h-1 bg-gray-800 rounded overflow-hidden" bis_skin_checked="1">
-            <div class="w-24 h-full bg-indigo-500" bis_skin_checked="1"></div>
-        </div>
-        <div class="flex flex-wrap sm:flex-row flex-col py-6 mb-12" bis_skin_checked="1">
-            <h1 class="sm:w-2/5 text-white font-medium title-font text-2xl mb-2 sm:mb-0">Space The Final Frontier</h1>
-            <p class="sm:w-3/5 leading-relaxed text-base sm:pl-10 pl-0">Street art subway tile salvia four dollar toast bitters selfies quinoa yuccie synth meditation iPhone intelligentsia prism tofu. Viral gochujang bitters dreamcatcher.</p>
-        </div>
-        </div>
-        <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4" bis_skin_checked="1">
-        <div class="p-4 md:w-1/3 sm:mb-0 mb-6" bis_skin_checked="1">
-            <div class="rounded-lg h-64 overflow-hidden" bis_skin_checked="1">
-            <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1203x503">
+    <div class="container px-5 py-24 mx-auto">
+        <h1 class="sm:text-3xl text-2xl font-medium title-font text-center text-white mb-20">Jadwal Misa
+            <br class="hidden sm:block">Gereja Santo Barnabas
+        </h1>
+        <div class="-my-8 divide-y-2 divide-gray-800">
+        <div class="py-8 flex flex-wrap md:flex-nowrap">
+            <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+            <span class="font-semibold title-font text-white">Misa Harian</span>
+            <span class="mt-1 text-gray-500 text-sm">Gereja Katolik Santo Barnabas</span>
             </div>
-            <h2 class="text-xl font-medium title-font text-white mt-5">Shooting Stars</h2>
-            <p class="text-base leading-relaxed mt-2">Swag shoindxgoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-            <a class="text-indigo-400 inline-flex items-center mt-3">Learn More
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-            </a>
-        </div>
-        <div class="p-4 md:w-1/3 sm:mb-0 mb-6" bis_skin_checked="1">
-            <div class="rounded-lg h-64 overflow-hidden" bis_skin_checked="1">
-            <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1204x504">
+            <div class="md:flex-grow">
+            <h2 class="text-2xl font-medium text-white title-font mb-2">Jam 06.00 WIB</h2>
+            <p class="leading-relaxed">Perayaan Ekaristi yang dilakukan setiap hari Senin - Jumat di Gereja Katolik Santo Barnabas.</p>
+            <!-- <a class="text-indigo-400 inline-flex items-center mt-4">Learn More
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M5 12h14"></path>
+                <path d="M12 5l7 7-7 7"></path>
+                </svg>
+            </a> -->
             </div>
-            <h2 class="text-xl font-medium title-font text-white mt-5">The Catalyzer</h2>
-            <p class="text-base leading-relaxed mt-2">Swag shoindxigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-            <a class="text-indigo-400 inline-flex items-center mt-3">Learn More
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-            </a>
         </div>
-        <div class="p-4 md:w-1/3 sm:mb-0 mb-6" bis_skin_checked="1">
-            <div class="rounded-lg h-64 overflow-hidden" bis_skin_checked="1">
-            <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1205x505">
+        <div class="py-8 flex border-t-2 border-gray-800 flex-wrap md:flex-nowrap">
+            <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+            <span class="font-semibold title-font text-white">Misa Jumat Pertama</span>
+            <span class="mt-1 text-gray-500 text-sm">Gereja Katolik Santo Barnabas</span>
             </div>
-            <h2 class="text-xl font-medium title-font text-white mt-5">The 400 Blows</h2>
-            <p class="text-base leading-relaxed mt-2">Swag shoindegoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
-            <a class="text-indigo-400 inline-flex items-center mt-3">Learn More
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-            </a>
+            <div class="md:flex-grow">
+            <h2 class="text-2xl font-medium text-white title-font mb-2">Jam 18.00 WIB</h2>
+            <p class="leading-relaxed">Perayaan Ekaristi yang dilakukan pada setiap awal bulan di Gereja Katolik Santo Barnabas.</p>
+            <!-- <a class="text-indigo-400 inline-flex items-center mt-4">Learn More
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M5 12h14"></path>
+                <path d="M12 5l7 7-7 7"></path>
+                </svg>
+            </a> -->
+            </div>
+        </div>
+        <div class="py-8 flex border-t-2 border-gray-800 flex-wrap md:flex-nowrap">
+            <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+            <span class="font-semibold title-font text-white">Misa Sabtu</span>
+            <span class="mt-1 text-gray-500 text-sm">Gereja Katolik Santo Barnabas</span>
+            </div>
+            <div class="md:flex-grow">
+            <h2 class="text-2xl font-medium text-white title-font mb-2">Jam 17.00 WIB</h2>
+            <p class="leading-relaxed">Perayaan Ekaristi yang dilakukan setiap Sabtu di Gereja Katolik Santo Barnabas.</p>
+            <!-- <a class="text-indigo-400 inline-flex items-center mt-4">Learn More
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M5 12h14"></path>
+                <path d="M12 5l7 7-7 7"></path>
+                </svg>
+            </a> -->
+            </div>
+        </div>
+        <div class="py-8 flex border-t-2 border-gray-800 flex-wrap md:flex-nowrap">
+            <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+            <span class="font-semibold title-font text-white">Misa Minggu</span>
+            <span class="mt-1 text-gray-500 text-sm">Gereja Katolik Santo Barnabas</span>
+            </div>
+            <div class="md:flex-grow">
+            <h2 class="text-2xl font-medium text-white title-font mb-2">Jam 05.30 WIB</h2>
+            <h2 class="text-2xl font-medium text-white title-font mb-2">Jam 07.30 WIB</h2>
+            <h2 class="text-2xl font-medium text-white title-font mb-2">Jam 10.00 WIB</h2>
+            <h2 class="text-2xl font-medium text-white title-font mb-2">Jam 17.00 WIB</h2>
+            <p class="leading-relaxed">Perayaan Ekaristi yang dilakukan setiap Minggu di Gereja Katolik Santo Barnabas.</p>
+            <!-- <a class="text-indigo-400 inline-flex items-center mt-4">Learn More
+                <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M5 12h14"></path>
+                <path d="M12 5l7 7-7 7"></path>
+                </svg>
+            </a> -->
+            </div>
+        </div>
+    </div>
+    <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-wrap w-full mb-20">
+        <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">Warta Paroki</h1>
+            <div class="h-1 w-20 bg-indigo-500 rounded"></div>
+        </div>
+        <p class="lg:w-1/2 w-full leading-relaxed text-gray-400 text-opacity-90">Selamat datang di edisi terbaru Warta Paroki Santo Barnabas! Dalam bulan yang penuh berkat ini, kami ingin berbagi berbagai kegiatan dan peristiwa yang mengisi hidup berjemaat. Gereja Santo Barnabas terus menjadi tempat yang penuh kasih dan rohaniah, menginspirasi umat untuk hidup dalam iman dan pelayanan.</p>
+        </div>
+        <div class="flex flex-wrap -m-4">
+        <div class="xl:w-1/4 md:w-1/2 p-4">
+            <div class="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
+            <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content">
+            <h3 class="tracking-widest text-indigo-400 text-xs font-medium title-font">SUBTITLE</h3>
+            <h2 class="text-lg text-white font-medium title-font mb-4">Chichen Itza</h2>
+            <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+            </div>
+        </div>
+        <div class="xl:w-1/4 md:w-1/2 p-4">
+            <div class="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
+            <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content">
+            <h3 class="tracking-widest text-indigo-400 text-xs font-medium title-font">SUBTITLE</h3>
+            <h2 class="text-lg text-white font-medium title-font mb-4">Colosseum Roma</h2>
+            <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+            </div>
+        </div>
+        <div class="xl:w-1/4 md:w-1/2 p-4">
+            <div class="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
+            <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/722x402" alt="content">
+            <h3 class="tracking-widest text-indigo-400 text-xs font-medium title-font">SUBTITLE</h3>
+            <h2 class="text-lg text-white font-medium title-font mb-4">Great Pyramid of Giza</h2>
+            <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+            </div>
+        </div>
+        <div class="xl:w-1/4 md:w-1/2 p-4">
+            <div class="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
+            <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/723x403" alt="content">
+            <h3 class="tracking-widest text-indigo-400 text-xs font-medium title-font">SUBTITLE</h3>
+            <h2 class="text-lg text-white font-medium title-font mb-4">San Francisco</h2>
+            <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+            </div>
         </div>
         </div>
     </div>
