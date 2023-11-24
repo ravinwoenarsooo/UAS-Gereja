44221eec3716b33c4937d3e3bd20d62e9f0e9bcb
killmeplsz <129037217+killmeplsz@users.noreply.github.com>
2023-11-24 19:04:41 +0700

    ga

--- UAS/resources/views/robertus.blade.php
+++ UAS/resources/views/robertus.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.home')
 
 @section('content')
+<div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap w-full mb-20">
+        <div class="w-full mb-6 lg:mb-0">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-black">Profil</h1>
+            <div class="h-1 w-20 bg-yellow-950 rounded"></div>
+            <p class="mt-4 leading-relaxed text-black text-opacity-90">
+            Seiring berjalannya waktu, dan di dukung dengan peran serta umat Paroki St. Agustinus, Karawaci, maka penantian akan gedung gereja yang layak dapat terealisasikan setelah Paroki St. Agustinus, Karawaci berdiri selama hampir 25 tahun. Pembangunan di awali dengan pendirian Gedung Karya Pastoral (GKP) yang kemudian dilanjutkan pada tahap pembangunan kembali atau renovasi Aula Strada Slamet Riyadi yang selama ini digunakan oleh umat Katolik Karawaci dalam Beribadat.
+            </p>
+            <p class="mt-4 leading-relaxed text-black text-opacity-90">
+            Seiring berjalannya waktu, dan di dukung dengan peran serta umat Paroki St. Agustinus, Karawaci, maka penantian akan gedung gereja yang layak dapat terealisasikan setelah Paroki St. Agustinus, Karawaci berdiri selama hampir 25 tahun. Pembangunan di awali dengan pendirian Gedung Karya Pastoral (GKP) yang kemudian dilanjutkan pada tahap pembangunan kembali atau renovasi Aula Strada Slamet Riyadi yang selama ini digunakan oleh umat Katolik Karawaci dalam Beribadat.
+            </p>
+            <p class="mt-4 leading-relaxed text-black text-opacity-90">
+            Seiring berjalannya waktu, dan di dukung dengan peran serta umat Paroki St. Agustinus, Karawaci, maka penantian akan gedung gereja yang layak dapat terealisasikan setelah Paroki St. Agustinus, Karawaci berdiri selama hampir 25 tahun. Pembangunan di awali dengan pendirian Gedung Karya Pastoral (GKP) yang kemudian dilanjutkan pada tahap pembangunan kembali atau renovasi Aula Strada Slamet Riyadi yang selama ini digunakan oleh umat Katolik Karawaci dalam Beribadat.
+            </p>
+        </div>
+    </div>
+</div>
 
+
+        
 @endsection
