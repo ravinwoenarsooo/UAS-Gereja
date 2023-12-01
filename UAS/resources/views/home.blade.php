ba65aed2bdc306ffab24ff9d24e74878e5dc4344
ravinwoenarsooo <robertus.ravin@student.umn.ac.id>
2023-12-01 20:54:13 +0700

    Update home.blade.php

--- UAS/resources/views/home.blade.php
+++ UAS/resources/views/home.blade.php
@@ -1,43 +1,39 @@
 @extends('layouts.home')
 
 @section('content')
-<div>
-<section class="text-black body-font" style="background-image: url('{{ asset('/images/image_5.svg') }}'); background-size: cover;">
-            <div class="container px-10 py-54 mx-auto z-0">
-                    <div class="max-w-6xl mx-auto my-auto z-0">
-                        <div id="default-carousel" class="relative" data-carousel="static">
-                            <div class="overflow-hidden relative h-screen rounded-lg sm:h-64 xl:h-80 2xl:h-96">
-                                <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                                    <img src="{{ asset('images/perayaan2.jpg') }}" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="slide 1">
-                                </div>
-                                <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                                    <img src="{{ asset('images/perayaan.jpg') }}" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="slide 2">
-                                </div>
-                                <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                                    <img src="{{ asset('images/perayaan3.jpg') }}" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="slide 3">
-                                </div>
-                            </div>
-                            <div class="flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2">
-                                <button type="button" class="w-3 h-3 rounded-full " aria-current="false" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-                                <button type="button" class="w-3 h-3 rounded-full " aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-                                <button type="button" class="w-3 h-3 rounded-full " aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-                            </div>
-                        
-                            <button type="button" class="flex absolute top-0 left-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-prev>
-                            <span class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30  group-focus:ring-4 group-focus:ring-white  group-focus:outline-none">
-                                <svg class="w-5 h-5 text-white sm:w-6 sm:h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
-                                <span class="hidden">Anterior</span>
-                            </span>
-                        </button>
-                        <button type="button" class="flex absolute top-0 right-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-next>
-                            <span class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 0 group-focus:ring-4 group-focus:ring-white  group-focus:outline-none">
-                                <svg class="w-5 h-5 text-white sm:w-6 sm:h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
-                                <span class="hidden">Siguiente</span>
-                            </span>
-                        </button>
+<section class="text-black body-font">
+    <div style="background-image: url('{{ asset('/images/image_5.svg') }}'); background-size: cover">
+        <div class="container px-10 py-54 h-screen mx-auto z-0">
+            <div class="max-w-6xl mx-auto z-0" >
+                <div id="default-carousel" class="relative" data-carousel="static">
+                    <div class="overflow-hidden mx-auto relative h-screen rounded-lg">
+                        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                            <iframe width="1280" height="720" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" src="https://www.youtube.com/embed/sIgKYiRqYpQ" frameborder="0" allowfullscreen></iframe>
+                        </div>
+                        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                            <iframe width="1280" height="720" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" src="https://www.youtube.com/embed/XslUq9Q0CAs" frameborder="0" allowfullscreen></iframe>
                         </div>
                     </div>
+                    <div class="flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2">
+                        <button type="button" class="w-3 h-3 rounded-full " aria-current="false" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+                        <button type="button" class="w-3 h-3 rounded-full " aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+                    </div>
+
+                    <button type="button" class="flex absolute top-0 left-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-prev>
+                        <span class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30  group-focus:ring-4 group-focus:ring-white  group-focus:outline-none">
+                            <svg class="w-5 h-5 text-white sm:w-6 sm:h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
+                            <span class="sr-only">Anterior</span>
+                        </span>
+                    </button>
+                    <button type="button" class="flex absolute top-0 right-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-next>
+                        <span class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 0 group-focus:ring-4 group-focus:ring-white  group-focus:outline-none">
+                            <svg class="w-5 h-5 text-white sm:w-6 sm:h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
+                            <span class="sr-only">Siguiente</span>
+                        </span>
+                    </button>
                 </div>
+            </div>
+        </div>
     </div>
     <div class="container px-5 py-24 mx-auto" bis_skin_checked="1">
         <div class="flex flex-col text-center w-full mb-20" bis_skin_checked="1">
